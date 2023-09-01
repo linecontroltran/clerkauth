43d34f65591e4f123a76ed05457bcf60ee5fd295
Tom <linecontroltran@gmail.com>
2023-09-01 12:34:18 -0400

    Version 1 of clerk/nextjs

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+    <h1 className="text-2x1 font-bold mb-5">Welcome</h1>
+    <p className="mb-5">
+      This is a demo site.
+      </p>
+    </>
+  );
+}
